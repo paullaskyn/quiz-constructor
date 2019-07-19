ba385b136d3d5546030380c3ef72e485265c7969
paullaskyn <kirill-nikolaenko@mail.ua>
2019-07-19 06:32:35 +0300

    content for profile page

--- src/content/admin/profile.php
+++ src/content/admin/profile.php
@@ -1,10 +1,38 @@
 <div class="container htu-text-block content-start">
-	<h2>Profile page</h2><br>
+	<h2>Profile</h2>
+	<div class="data_change_forms">
+		<form id="userdataChange">
+			<div class="form-row">
+				<div class="form-group col-md-6">
+					<label for="inputName">Name</label>
+					<input type="text" class="form-control" id="inputName" name="pd_name" placeholder="Name" value="<?php echo $user_name?>" autocomplete="off">
+				</div>
+				<!--<div class="form-group col-md-6">
+					<label for="inputEmail">Email</label>
+					<input type="email" class="form-control" id="inputEmail" name="pd_email" placeholder="Email" value="<?php #echo $user_email?>" autocomplete="off">
+				</div>-->
+			</div>
+			<button type="submit" class="btn btn-primary">Save data</button>
+		</form>
 
-	<?= $controller ?><br>
-	<?= $page ?> <br><br>
-
-
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<form id="passwordChange">
+			<div class="form-row">
+				<input type="email" class="hidden" name="email" value="<?php #echo $profile['user_email']?>">
+				<div class="form-group col-md-6">
+					<label for="inputPassword1">Old password</label>
+					<input type="password" class="form-control" id="inputPassword1" name="pd_pass_old" placeholder="Old password" autocomplete="new-password">
+				</div>
+				<div class="form-group col-md-6">
+					<label for="inputPassword2">New password</label>
+					<input type="password" class="form-control" id="inputPassword2" name="pd_pass_new" placeholder="New password" autocomplete="off">
+				</div>
+				<div class="form-group col-md-6">
+					<label for="inputPassword3">Repeat new password</label>
+					<input type="password" class="form-control" id="inputPassword3" name="pd_pass_new_rec" placeholder="Repeat new password" autocomplete="off">
+				</div>
+			</div>
+			<button type="submit" class="btn btn-primary">Change password</button>
+		</form>
+		<div class="alert alert-danger" role="alert"></div>
+	</div>
 </div>
