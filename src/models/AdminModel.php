a1bb590777c3ea0bd0d7ce345c26b8bfb7b9d138
paullaskyn <kirill-nikolaenko@mail.ua>
2019-07-23 22:05:41 +0300

    .

--- src/models/AdminModel.php
+++ src/models/AdminModel.php
@@ -1,25 +1,29 @@
 <?php
 
-	namespace models;
+    namespace models;
 
-	class AdminModel
-	{
+    class AdminModel
+    {
 
-		/**
-		 * If the user performed the operation(signup, signin, recover, newpassword),
-		 * we start data checking and running operation method from class Account
-		 *
-		 * @method __construct
-		 */
+        private $quiz_config;
 
-		public function __construct()
-		{
-			if (isset($_POST['operation'])){
+        /**
+         * If the user performed the operation(signup, signin, recover, newpassword),
+         * we start data checking and running operation method from class Account
+         *
+         * @method __construct
+         */
 
-				userdata\DataValidation::startValidate();
+        public function __construct()
+        {
+            if (isset($_POST['operation'])){
+                userdata\DataValidation::startValidate();
+                $account = new userdata\Account($_POST['operation'] . 'Operation');
+            }
 
-				$account = new userdata\Account($_POST['operation'] . 'Operation');
-			}
-		}
-
-	}
+            elseif (isset($_POST['quiz_operation'])){
+                $this->quiz_config =  new quizCreation\quizArrayBuild();
+                $addtodb = new quizCreation\addQuizToDb($this->quiz_config->output_array);
+            }
+        }
+    }
